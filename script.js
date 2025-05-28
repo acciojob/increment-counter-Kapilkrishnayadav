1e730b5e681d5562b6c6b93fd9277acb67f09bf4
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-28 22:10:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+const btn=document.getElementById("incrementBtn");
+        const counter=document.getElementById("counter");
+        let c=0;
+        btn.addEventListener("click",()=>{
+            alert(c);
+            c++;
+            counter.innerText=c;
+        })
